d832bd693db53d9e4b3e406fe203cf60deba0b9c
Mark Turner <markjturner@gmail.com>
2011-03-16 09:52:13 +0000

    Fix for google 'email already taken' bug

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,24 +4,35 @@ class AuthenticationsController < ApplicationController
   end
     
   def create
+    # this is complicated... check the comments below
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
-    if authentication   # sign in if service associated with logged out user
+    if authentication   
+      # sign in if service associated with logged out user
       flash[:notice] = "Signed in successfully."
       sign_in_and_redirect(:user, authentication.user)
-    elsif current_user  # add service to current logged in user
+    elsif current_user  
+      # add service to current logged in user
       current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
       flash[:notice] = "Authentication successful."
       redirect_to authentications_url
-    else    # register new user
-      user = User.new
-      user.apply_omniauth(omniauth)
-      if user.save
-        flash[:notice] = "Signed in successfully."
+    else
+      if user = User.find_by_email(omniauth['user_info']['email'])  
+        # check if user exists and associate service and log them in if so (fix for google 'email already taken' bug)
+        user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+        flash[:notice] = "Authentication successful."
         sign_in_and_redirect(:user, user)
-      else
-        session[:omniauth] = omniauth.except('extra')
-        redirect_to new_user_registration_url
+      else  
+        # if user doesn't exist, register new user
+        user = User.new
+        user.apply_omniauth(omniauth)
+        if user.save
+          flash[:notice] = "Signed in successfully."
+          sign_in_and_redirect(:user, user)
+        else
+          session[:omniauth] = omniauth.except('extra')
+          redirect_to new_user_registration_url
+        end
       end
     end
   end
